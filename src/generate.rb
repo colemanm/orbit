c1b1febbee813c5ebb107bb83cafc9cab127b75c
Coleman McCormick <cmccormick@gmail.com>
2014-03-04 22:48:52 -0500

    Script to generate site

--- src/generate.rb
+++ src/generate.rb
@@ -2,3 +2,26 @@
 
 require 'thor'
 require 'haml'
+
+class Orbit < Thor
+
+  desc "site", "Generate main index page and subpages."
+  def site
+    files = Dir.glob('../images/thumbs/*')
+    files.each { |file| file.sub!(/../, '') }
+
+    content = "---\nlayout: default\n---\n\n"
+    files.each do |file|
+      content << "<div class='image'><img src='#{file}' /></div>\n"
+    end
+
+    puts content
+
+    File.open("../index.html", "wt") do |f|
+      f.write(content)
+    end
+  end
+
+end
+
+Orbit.start
