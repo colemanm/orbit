596cfd913c7e302faf7574bc693f8697b011af60
Coleman McCormick <cmccormick@gmail.com>
2014-03-04 23:12:26 -0500

    More reorg, map locations

--- src/generate.rb
+++ src/generate.rb
@@ -2,6 +2,10 @@
 
 require 'thor'
 require 'haml'
+require 'maruku'
+require 'find'
+require 'pathname'
+require 'nokogiri'
 
 class Orbit < Thor
 
@@ -12,14 +16,30 @@ class Orbit < Thor
 
     content = "---\nlayout: default\n---\n\n"
     files.each do |file|
-      content << "<div class='image'><img src='#{file}' /></div>\n"
+      content << "<div class='image'><a href='place/'><img src='#{file}' /></a></div>\n"
     end
 
-    puts content
+    # puts content
 
-    File.open("../index.html", "wt") do |f|
-      f.write(content)
+    # File.open("../index.html", "wt") do |f|
+    #   f.write(content)
+    # end
+
+    puts parse_title("glaciers")
+  end
+
+  no_tasks do
+
+    def parse_title(dir)
+      page = Nokogiri::HTML::Document.parse(Maruku.new(File.read("../images/#{dir}/README.md")).to_html)
+      title = page.at_css "h1"
+      title.content
     end
+
+    def generate_page(title)
+
+    end
+
   end
 
 end
