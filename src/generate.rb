56e7cdb476c05c22f60a1f549c18a1766000539c
Coleman McCormick <cmccormick@gmail.com>
2014-03-13 18:22:07 -0400

    add titles + descriptions

--- src/generate.rb
+++ src/generate.rb
@@ -25,18 +25,21 @@ class Orbit < Thor
     end
 
     files.each do |file|
-      path = "../place/#{basename(file)}/"
+      loc = basename(file)
+      path = "../place/#{loc}/"
 
       FileUtils.mkdir_p(path) unless File.directory?(path)
       File.open("#{path}index.html", "wt") do |f|
         place_content = "---\nlayout: detail\n---\n\n"
         place_content <<
 "<div>
-  <img class='screenshot' src='../../images/#{basename(file)}/screenshot.jpg' />
+  <img class='screenshot' src='../../images/#{loc}/screenshot.jpg' />
+  <p class='title'>#{parse_title(loc)}</p>
+  <p class='description'>#{parse_description(loc)}</p>
   <ul class='downloads'>
-    <li><a href='../../images/#{basename(file)}/iphone5-#{basename(file)}.jpg'>iPhone 5</a></li> <span class='bullet'>&bull;</span>
-    <li><a href='../../images/#{basename(file)}/iphone4-#{basename(file)}.jpg'>iPhone 4</a></li> <span class='bullet'>&bull;</span>
-    <li><a href='../../images/#{basename(file)}/ipad-#{basename(file)}.jpg'>iPad</a></li>
+    <li><a href='../../images/#{loc}/iphone5-#{loc}.jpg'>iPhone 5</a></li> <span class='bullet'>&bull;</span>
+    <li><a href='../../images/#{loc}/iphone4-#{loc}.jpg'>iPhone 4</a></li> <span class='bullet'>&bull;</span>
+    <li><a href='../../images/#{loc}/ipad-#{loc}.jpg'>iPad</a></li>
   </ul>
 </div>"
         f.write(place_content)
@@ -52,8 +55,10 @@ class Orbit < Thor
       title.content
     end
 
-    def generate_page(title)
-
+    def parse_description(dir)
+      page = Nokogiri::HTML::Document.parse(Maruku.new(File.read("../images/#{dir}/README.md")).to_html)
+      description = page.at_css "p"
+      description.content
     end
 
     def basename(file)
